6c5786823607f433ae66fec4f62deea6afa32d08
lazlanrafar <lazlanrafar@gmail.com>
2022-03-30 12:43:22 +0700

    update category

--- app/category/controller.js
+++ app/category/controller.js
@@ -3,7 +3,10 @@ const Category = require("./model");
 module.exports = {
   index: async (req, res) => {
     try {
-      res.render("admin/category/view_category");
+      const category = await Category.find();
+      res.render("admin/category/view_category", {
+        category,
+      });
     } catch (error) {
       console.log(error);
     }
@@ -20,6 +23,27 @@ module.exports = {
       const { name } = req.body;
       const category = await Category({ name });
       await category.save();
+
+      res.redirect("/category");
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  viewEdit: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const category = await Category.findById(id);
+      res.render("admin/category/edit", {
+        category,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  actionEdit: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const category = await Category.findByIdAndUpdate(id, req.body);
 
       res.redirect("/category");
     } catch (error) {
